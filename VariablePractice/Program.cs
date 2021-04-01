b7686e05d1d9a77ce2838ccc974ffec3867e7503
David Harris <davlsu14@gmail.com>
2021-04-01 15:00:56 -0500

    Variables Practice upload

---
+++ VariablePractice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VariablePractice
+{
+    
+    
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                string dogName = "Tripp";
+                int dogAge = 5;
+                char nickName = 'T';
+                bool lovestoPlay = true;
+                double tennisBalls = 30;
+                decimal weight = 75.3m;
+
+                Console.WriteLine($"My dogs name is {dogName}. He is {dogAge} years old and has {tennisBalls} tennisballs and it is {lovestoPlay} that he loves to play, which keeps him at his weight of {weight} pounds! ");
+
+            }
+        }
+    }
+
+    
+
